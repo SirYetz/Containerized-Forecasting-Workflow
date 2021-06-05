4e82f3083d863fcc7cc80418a4f2189fa08c0667
MrDoesNotUnderstand <72126684+MrDoesNotUnderstand@users.noreply.github.com>
2021-06-05 20:58:14 +1000

    20 runs

--- LHS/run_lhs.py
+++ LHS/run_lhs.py
@@ -500,7 +500,7 @@ def move_files(n):
 
 if __name__ == '__main__':
     try:
-        runs = 2
+        runs = 20
 
         #Just testing functions at this stage
         #Use this once testing of each function complete
