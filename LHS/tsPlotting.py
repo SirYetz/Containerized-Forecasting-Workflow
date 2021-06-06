15d0042a74ea92a27a56093f3882e4d300a902fa
MrDoesNotUnderstand <72126684+MrDoesNotUnderstand@users.noreply.github.com>
2021-06-06 11:43:01 +1000

    Adding observed windspeed and final changes to tsPlotting
    Adding text file which contains extracted u wind speed for 2011/12/31
    Changes to tsPlotting to ensure it can access correct files from Tim's PC

--- LHS/tsPlotting.py
+++ LHS/tsPlotting.py
@@ -3,11 +3,13 @@ import bisect
 import matplotlib.pyplot as plt
 import os
 import math
+import matplotlib.patches as mpatches
 
 
-file = 'C:\\Users\\jhmil\\trit.d01.TS'
-data = 'C:\\Users\\jhmil\\clientdata.txt'
-#file = '/home/student/run/trit.d01.TS'
+#file = 'C:\\Users\\jhmil\\TS Files\\5 runs\\trit.d01_5run.TS'
+#data = 'C:\\Users\\jhmil\\clientdata.txt'
+file = '/home/student/run/trit.d01.TS'
+data = '/home/student/Containerized-Forecasting-Workflow/LHS/clientdata.txt'
 #reads file and delete the first irrelevant line of text
 with open(file,'r') as f:
     lines = f.readlines()
@@ -17,48 +19,35 @@ with open(file + ".out", 'w')as f:
     for line in lines:
         f.writelines(line)
 
-file2 = 'C:\\Users\\jhmil\\trit.d01.TS.out'
-#file2 = '/home/student/run/trit.d01.TS.out'
+#file2 = 'C:\\Users\\jhmil\\TS Files\\trit.d01.TS.out'
+file2 = '/home/student/run/trit.d01.TS.out'
 #loads u wind component to array
 xPoints = (np.loadtxt(file2)[:,7])
-#loads v wind componenet to array
-yPoints = (np.loadtxt(file2)[:,8])
-#Create array which will be used to store magnitude of wind
-windMagnitude = []
-#Create array which acts as a coutner to track time steps
-timeStep = []
-clientData = (np.loadtxt(data))
-#parse arrays of u and v components to determine true magnitude of wind speed 
-for i in range(len(xPoints)):
-    u = xPoints[i]
-    v = yPoints[i]
-    magnitude = math.sqrt(u**2+v**2)
-    windMagnitude.append(magnitude)
-    #timeStep.append(i)
-windTimeStep = 24/len(windMagnitude)
+#loads observed wind speed from file
+clientData = (np.loadtxt(data)) 
+#This is neccessary to ensure the predicted wind speed and observed have the same time steps
+windTimeStep = 24/len(xPoints)
 xWind = 0
 xWindArray =[]
-for i in range(len(windMagnitude)):
+for i in range(len(xPoints)):
     xWindArray.append(xWind)
     xWind = xWind+windTimeStep
+#As per previous comment
 clientTimeStep = 24/len(clientData)
 xClient = 0
 xClientArray = []
 for i in range(len(clientData)):
     xClientArray.append(xClient)
     xClient = xClient+clientTimeStep
-#print(xClientArray)
-#print(xWindArray)
-#print(xWind)
-#print(xClient)
-#print (windMagnitude)
-#print (timeStep)
-#print(clientData)
+
 
 #plot data
-plt.plot(xWindArray, windMagnitude)
-plt.plot(xClientArray, clientData)
+plt.plot(xWindArray, xPoints,"blue")
+plt.plot(xClientArray, clientData,"red")
 plt.title("Wind Magnitude")
-plt.xlabel("Time Step")
-plt.ylabel("Wind velocity ms-1")
+plt.xlabel("Time (hr)")
+plt.ylabel("Wind velocity (ms-1)")
+red_patch = mpatches.Patch(color='red', label='Client Data')
+blue_patch = mpatches.Patch(color='blue', label='Predicted Wind Speed')
+plt.legend(handles=[red_patch, blue_patch])
 plt.show()
